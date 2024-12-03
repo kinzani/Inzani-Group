10daf478bfd659800faa5145be89b6a08e7610fe
Dan Criveanu <dan.criveanu25@gmail.com>
2024-12-03 14:24:22 +0000

    Updated get_band_gap.py

--- HelperScripts/VASPstuff/get_band_gap.py
+++ HelperScripts/VASPstuff/get_band_gap.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 from pymatgen.io.vasp.outputs import Eigenval
-band_gap, cbm, vbm, is_band_gap_direct = Eigenval("EIGENVAL").eigenvalue_band_properties
+try:
+    band_gap, cbm, vbm, is_band_gap_direct = Eigenval("EIGENVAL").eigenvalue_band_properties
 
-print(f"band_gap: {band_gap} eV")
-print(f"cbm: {cbm} eV")
-print(f"vbm: {vbm} eV")
-print(f"is_band_gap_direct: {is_band_gap_direct}")
+    print(f"band_gap: {band_gap} eV")
+    print(f"cbm: {cbm} eV")
+    print(f"vbm: {vbm} eV")
+    print(f"is_band_gap_direct: {is_band_gap_direct}")
+except:
+    print("Script failed. Does this directory have an EIGENVAL file to read?")
